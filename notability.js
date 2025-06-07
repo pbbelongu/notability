e5b80d075cfed4ee24b2cd6f61354c01349537e8
Kang <103311946+pbbelongu@users.noreply.github.com>
2025-06-07 18:34:09 +0800

    Update notability.js

--- notability.js
+++ notability.js
@@ -1,22 +1,3 @@
-/*************************************
-
-项目名称：Notability
-下载地址：https://t.cn/A6Cgjtei
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-
-**************************************
-
-[rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/notability.js
-
-[mitm]
-hostname = notability.com
-
-*************************************/
-
-
 var chxm1023 = JSON.parse($response.body);
 
 chxm1023 = {
