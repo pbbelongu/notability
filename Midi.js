a88672aebe6c308f337b52e278e24aeb2dcc8f65
Kang <103311946+pbbelongu@users.noreply.github.com>
2025-12-26 04:07:40 +0800

    更新 Midi.js

--- Midi.js
+++ Midi.js
@@ -1,28 +1,22 @@
-// 取得原始回傳數據
-let body = $response.body;
-if (!body) $done({}); // 如果沒有 body 則直接跳過
-
-let obj = JSON.parse(body);
-
-// 針對 md.studio 域名常見的數據結構進行修改
-// 假設權限在 data 或 user 欄位中
-const modify = (item) => {
-    if (typeof item !== 'object' || item === null) return;
-
-    // 修改常見的會員開關
-    if (item.hasOwnProperty('is_vip')) item.is_vip = true;
-    if (item.hasOwnProperty('vip_type')) item.vip_type = 1;
-    if (item.hasOwnProperty('is_premium')) item.is_premium = true;
+// Midi.js
+if ($response.body) {
+    let obj = JSON.parse($response.body);
     
-    // 修改過期時間 (2099-12-31)
-    if (item.hasOwnProperty('expire_time')) item.expire_time = 4070880000;
-    if (item.hasOwnProperty('expires_at')) item.expires_at = 4070880000;
-};
+    // 修改用戶狀態
+    if (obj.user) {
+        obj.user.vip_type = 1; 
+        obj.user.is_vip = true;
+        obj.user.expire_time = 4070880000; // 2099年
+    }
+    
+    // 有些版本數據在 data 層
+    if (obj.data) {
+        obj.data.is_premium = true;
+        obj.data.vip_status = 1;
+        obj.data.expiration_date = "2099-12-31T23:59:59Z";
+    }
 
-// 執行修改
-modify(obj);
-if (obj.data) modify(obj.data);
-if (obj.user) modify(obj.user);
-
-// 回傳修改後的結果
-$done({ body: JSON.stringify(obj) });
+    $done({body: JSON.stringify(obj)});
+} else {
+    $done({});
+}
